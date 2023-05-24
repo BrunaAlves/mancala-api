b1866ae38c2e8ff82c9c167576078c4f26ddc031
BrunaAlves <brunahalves94@gmail.com>
2023-05-25 00:35:47 +0200

    Add unit test

--- src/test/java/com/game/mancala/service/MancalaServiceTest.java
+++ src/test/java/com/game/mancala/service/MancalaServiceTest.java
@@ -2,14 +2,13 @@ package com.game.mancala.service;
 
 import com.game.mancala.dao.IMancalaGameDAO;
 import com.game.mancala.dao.MancalaGameDAO;
-import com.game.mancala.dto.GameActionsDTO;
 import com.game.mancala.exception.MancalaException;
 import com.game.mancala.model.*;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -260,7 +259,7 @@ public class MancalaServiceTest {
                 mancalaService.startGame(Arrays.asList("p1", "p2"), 6, 6);
 
         //when
-        GameActionsDTO gameActionsDTO = mancalaService
+        mancalaService
                 .getActions(mancalaGame.getCurrentPlayer().getPits().get(0).getId());
 
         MancalaGame getMancalaGame = mancalaService.getGame();
@@ -293,6 +292,26 @@ public class MancalaServiceTest {
     }
 
     @Test
+    public void should_notAddStonesToOpponentLargePit_whenMakeTheMoves() {
+        //given
+        MancalaGame mancalaGame =
+                mancalaService.startGame(Arrays.asList("p1", "p2"), 2, 10);
+
+        //when
+        Player firstPlayer = mancalaGame.getCurrentPlayer();
+        mancalaService.getActions(mancalaGame.getCurrentPlayer().getPits().get(0).getId());
+
+
+        int totalOpponentsLargePitSum = mancalaGame.getPlayers()
+                .stream()
+                .filter(x -> x.getId() != firstPlayer.getId())
+                .mapToInt(x -> x.getLargePit().getStones())
+                .sum();
+        //then
+        Assertions.assertEquals(totalOpponentsLargePitSum, 0);
+    }
+
+    @Test
     public void should_beOpponentPlayerTime_whenLastStoneDroppedInTheOpponentPit() {
         //given
         MancalaGame mancalaGame =
@@ -308,50 +327,55 @@ public class MancalaServiceTest {
         Assertions.assertNotEquals(firstPlayer.getId(), playerToBeChecked.getId());
     }
 
+    @Test
+    public void should_returnTieAction_whenGameIsTied(){
+        MancalaGame mancalaGame = mancalaService.startGame(Arrays.asList("p1", "p2"), 1, 1);
+        List<Action> actions = mancalaService.getActions(mancalaGame.getCurrentPlayer().getPits().get(0).getId());
+
+        Assertions.assertEquals(actions.stream().filter(x -> x.getActionType() == ActionType.TIE).count(), 1);
+    }
+
 
     @Test
     public void should_returnGameOverAndWinner_whenLastAction() {
         //given
         MancalaGame mancalaGame =
-                mancalaService.startGame(Arrays.asList("p1", "p2"), 6, 6);
-
-        Player currentPlayer = mancalaGame.getCurrentPlayer();
-        Player opponentPlayer = mancalaGame.getPlayers().get(findOpponentIndex(mancalaGame));
-
-        int totalCurrentPlayerSum = 0 + currentPlayer.getLargePit().getStones();
-        for (int i = 0; i < currentPlayer.getPits().size() - 2 ; i++) {
-            Pit pit = mancalaGame.getCurrentPlayer().getPits().get(i);
-            totalCurrentPlayerSum += pit.getStones();
-            pit.setStones(0);
-        }
-
-        for (int i = 0; i < opponentPlayer.getPits().size() - 1 ; i++) {
-            Pit pit = opponentPlayer.getPits().get(i);
-            totalCurrentPlayerSum ++;
-            int totalStone = pit.getStones();
-            pit.setStones(totalStone - 1);
-        }
-
-        Pit lastPitToMove = currentPlayer.getPits().get(currentPlayer.getPits().size() - 2);
-        totalCurrentPlayerSum += lastPitToMove.getStones() - 1;
-        lastPitToMove.setStones(1);
-
-        mancalaGame.getCurrentPlayer().getLargePit().setStones(totalCurrentPlayerSum);
+                mancalaService.startGame(Arrays.asList("p1", "p2"), 2, 1);
 
         //when
-        GameActionsDTO gameActionsDTO = mancalaService.getActions(lastPitToMove.getId());
+        Player winnerPlayer = mancalaGame.getCurrentPlayer();
+        mancalaService.getActions(winnerPlayer.getPits().get(1).getId()); //A1
+        List<Action> actions = mancalaService.getActions(winnerPlayer.getPits().get(0).getId()); //b0
 
-        Optional<Action> gameOverAction = gameActionsDTO.getActions().stream()
-                .filter(x -> x.getActionType() == ActionType.GAME_OVER).findFirst();
-
-        Optional<Action> winnerAction = gameActionsDTO.getActions().stream()
+        Optional<Action> winnerAction = actions.stream()
                 .filter(x -> x.getActionType() == ActionType.WINNER).findFirst();
 
+        //then
+        Assertions.assertTrue(mancalaGame.isGameOver());
+        Assertions.assertEquals(actions.stream()
+                .filter(x -> x.getActionType() == ActionType.GAME_OVER).count(), 1);
+        Assertions.assertTrue(winnerAction.isPresent());
+        Assertions.assertEquals(winnerPlayer.getId(), ((WinnerAction) winnerAction.get()).getWinnerPlayerId());
+    }
+
+    @Test
+    public void should_returnCaptureStonesAction_whenTheOponnentPitIsEmpty() {
+        //given
+        MancalaGame mancalaGame =
+                mancalaService.startGame(Arrays.asList("p1", "p2"), 2, 1);
+
+        //when
+        Player winnerPlayer = mancalaGame.getCurrentPlayer();
+        mancalaService.getActions(winnerPlayer.getPits().get(1).getId()); //A1
+        List<Action> actions = mancalaService.getActions(winnerPlayer.getPits().get(0).getId()); //b0
+
+        CaptureStoneAction captureStonesAction = (CaptureStoneAction) actions.stream()
+                .filter(x -> x.getActionType() == ActionType.CAPTURE_STONES).findFirst().get();
 
         //then
-        Assertions.assertTrue(gameOverAction.isPresent());
-        Assertions.assertTrue(winnerAction.isPresent());
-        Assertions.assertEquals(currentPlayer.getId(), ((WinnerAction) winnerAction.get()).getWinnerPlayerId());
+        Assertions.assertEquals(captureStonesAction.getRemovedOwnPitStonesQuantity(), 1);
+        Assertions.assertEquals(captureStonesAction.getRemovedOppositeStonesQuantity(), 1);
+        Assertions.assertEquals(captureStonesAction.getAddedLargePitStonesQuantity(), 2);
     }
 
     private int findOpponentIndex(MancalaGame mancala) {
